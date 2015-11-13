12b3c9ce0c12d13221d69cfb5be176221638e145
skeshetti <skeshetti@paypal.com>
2015-11-12 22:46:20 -0600

    GemsUtil new file commited for testing

---
+++ GemsUtil.java
@@ -0,0 +1,38 @@
+package com.paypal.csp.infra.util;
+import weblogic.security.internal.SerializedSystemIni;
+import weblogic.security.internal.encryption.ClearOrEncryptedService;
+import weblogic.security.internal.encryption.EncryptionService;
+
+public class GemsUtil {
+	/**
+	 * Decrypt text in AES encrypted format using Weblogic decryption utilties.
+	 */
+	
+	
+	/**
+	 * @param encryptedString
+	 * @return decryptedString
+	 */
+	public static String decryptProperty(String encryptedString){
+		if(encryptedString == null)
+			return null;
+ 		String decryptedString = null;	
+			EncryptionService wlEncryptionService = SerializedSystemIni.getExistingEncryptionService();
+			if (wlEncryptionService != null){
+				ClearOrEncryptedService clearOrEncryptedService  = new ClearOrEncryptedService(wlEncryptionService);
+				decryptedString = clearOrEncryptedService.decrypt(encryptedString);
+			}
+		return decryptedString;		
+	}
+	
+	/*
+	 * This method used to replace the hyphens with underscores in a string
+	 */
+	public String replaceHyphensWithUnderscore(String key){
+		if(key.contains("-"))
+		{
+			key = key.replace("-", "_");
+		}
+		return key;
+	}
+}
